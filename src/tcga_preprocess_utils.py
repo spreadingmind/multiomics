c7aa738a8a2dd93d05f1ad889a3563101a1d668d
Elisabeth Shevtsova <liz@Elisabeths-MacBook-Air.local>
2023-12-10 19:03:29 +0300

    feat / refactor

--- src/tcga_preprocess_utils.py
+++ src/tcga_preprocess_utils.py
@@ -17,24 +17,43 @@ def handle_duplicates(df):
     return df
 
 
-def prepare_data(path, patient_ids):
+def prepare_data(path, patient_ids=None):
     data = pd.read_csv(path, sep=' ')
 
     data = data.T
     data.index = data.index.str.lower()
     data.index = data.index.str.replace(r'\.01$', '', regex=True)
 
-    data = data[data.index.isin(patient_ids)]
+    if patient_ids is not None:
+        data = data[data.index.isin(patient_ids)]
     return data
 
 
-def choose_common_patients(df1, df2, df3, df4):
-    common_indexes = set(df1.index) & set(
-        df2.index) & set(df3.index) & set(df4.index)
+def choose_common_patients(dfs):
+    if len(dfs) == 4:
+        df1, df2, df3, df4 = dfs
+        common_indexes = set(df1.index) & set(
+            df2.index) & set(df3.index) & set(df4.index)
 
-    df1_sync = df1[df1.index.isin(common_indexes)]
-    df2_sync = df2[df2.index.isin(common_indexes)]
-    df3_sync = df3[df3.index.isin(common_indexes)]
-    df4_sync = df4[df4.index.isin(common_indexes)]
+        df1_sync = df1[df1.index.isin(common_indexes)]
+        df2_sync = df2[df2.index.isin(common_indexes)]
+        df3_sync = df3[df3.index.isin(common_indexes)]
+        df4_sync = df4[df4.index.isin(common_indexes)]
 
-    return df1_sync, df2_sync, df3_sync, df4_sync
+        return df1_sync, df2_sync, df3_sync, df4_sync
+    elif len(dfs) == 6:
+        df1, df2, df3, df4, df5, df6 = dfs
+        common_indexes = set(df1.index) & set(
+            df2.index) & set(df3.index) & set(
+            df4.index) & set(df5.index) & set(
+            df6.index)
+        print(common_indexes)
+
+        df1_sync = df1[df1.index.isin(common_indexes)]
+        df2_sync = df2[df2.index.isin(common_indexes)]
+        df3_sync = df3[df3.index.isin(common_indexes)]
+        df4_sync = df4[df4.index.isin(common_indexes)]
+        df5_sync = df5[df5.index.isin(common_indexes)]
+        df6_sync = df6[df6.index.isin(common_indexes)]
+
+        return df1_sync, df2_sync, df3_sync, df4_sync, df5_sync, df6_sync
