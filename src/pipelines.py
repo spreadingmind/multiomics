ab00d68d0e42611cee8f2eb787bcc276ba2e1151
Elisabeth <lisabeth.shevtsova@gmail.com>
2024-03-03 17:37:03 +0300

    feat / add cox regression pipeline & evaluation

--- src/pipelines.py
+++ src/pipelines.py
@@ -1,9 +1,13 @@
+from sksurv.preprocessing import OneHotEncoder, encode_categorical
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from catboost import CatBoostClassifier, CatBoostRegressor
 from sklearn.metrics import roc_auc_score, roc_curve, mean_absolute_percentage_error
 from sksurv.metrics import concordance_index_censored
+from sksurv.linear_model import CoxPHSurvivalAnalysis
+from sklearn.pipeline import make_pipeline
+from sksurv.preprocessing import OneHotEncoder
 
 
 def classification_pipeline(
@@ -126,31 +130,34 @@ def regression_pipeline(
     X_train_factors, X_test_factors = factors_df.values[
         TRAIN_INDICES], factors_df.values[TEST_INDICES]
 
-    # Classifier for combined dataframe
-    cb_classifier_combined = CatBoostRegressor(
+    # Regressor for combined dataframe
+    cb_regressor_combined = CatBoostRegressor(
         n_estimators=5000, random_state=RANDOM_STATE, silent=True, cat_features=cat_features_indices)
-    cb_classifier_combined.fit(X_train, y_train)
+    cb_regressor_combined.fit(X_train, y_train)
 
-    # Classifier for factors-only dataframe
-    cb_classifier_factors = CatBoostRegressor(
+    # Regressor for factors-only dataframe
+    cb_regressor_factors = CatBoostRegressor(
         n_estimators=5000, random_state=RANDOM_STATE, silent=True)
-    cb_classifier_factors.fit(X_train_factors, y_train)
+    cb_regressor_factors.fit(X_train_factors, y_train)
 
     # Predictions and evaluations for combined dataframe
-    y_pred_combined = cb_classifier_combined.predict(X_test)
+    y_pred_combined = cb_regressor_combined.predict(X_test)
     # Predictions and evaluations for factors-only dataframe
-    y_pred_factors = cb_classifier_factors.predict(X_test_factors)
+    y_pred_factors = cb_regressor_factors.predict(X_test_factors)
 
+    event = survival_data_breast['Death'][TEST_INDICES].astype(bool)
     # metrics for factors-only dataframe
     mape_factors = mean_absolute_percentage_error(
         y_true=y_test, y_pred=y_pred_factors) * 100
-    c_index_factors = concordance_index_censored(survival_data_breast['Death'][TEST_INDICES].astype(bool), y_test, 1 / y_pred_factors)[0]
+    c_index_factors = concordance_index_censored(
+        event, y_test, 1 / y_pred_factors)[0]
 
     # metrics for combined dataframe
     mape_combined = mean_absolute_percentage_error(
         y_true=y_test, y_pred=y_pred_combined) * 100
-    c_index_combined = concordance_index_censored(survival_data_breast['Death'][TEST_INDICES].astype(bool), y_test, 1 / y_pred_combined)[0]
-    
+    c_index_combined = concordance_index_censored(
+        event, y_test, 1 / y_pred_combined)[0]
+
     metrics = {
         'MAPE_f': mape_factors,
         'MAPE_f_c': mape_combined,
@@ -163,7 +170,7 @@ def regression_pipeline(
     plt.figure(figsize=(20, 12))
 
     # Feature importances for factors-only dataframe
-    feature_importances_factors = cb_classifier_factors.get_feature_importance()
+    feature_importances_factors = cb_regressor_factors.get_feature_importance()
     importances_factors = pd.DataFrame({'Feature': [f'Factor {i+1}' for i in range(
         len(feature_importances_factors))], 'Importance': feature_importances_factors})
     importances_factors = importances_factors.sort_values(
@@ -177,7 +184,7 @@ def regression_pipeline(
     plt.ylabel('Importance')
 
     # Feature importances for combined dataframe
-    feature_importances_combined = cb_classifier_combined.get_feature_importance()
+    feature_importances_combined = cb_regressor_combined.get_feature_importance()
     feature_names = [
         f'Factor {i+1}' for i in range(N_FACTORS)] + CLINICAL_FEATURES
     importances_combined = pd.DataFrame(
@@ -195,4 +202,120 @@ def regression_pipeline(
     plt.tight_layout()
     plt.show()
 
+    return metrics
+
+
+def cox_regression_pipeline(
+        factors, clinical_features, survival_data_breast, TRAIN_INDICES, TEST_INDICES,
+        RANDOM_STATE, N_FACTORS, N_NUMERIC_CLINICAL, CLINICAL_FEATURES, plot_feat_imp=False):
+    # Original combined dataframe
+    combined_df = pd.concat([pd.DataFrame(factors), clinical_features], axis=1)
+    cat_features_indices = list(
+        range(N_FACTORS + N_NUMERIC_CLINICAL, combined_df.shape[1]))
+
+    # Factors-only dataframe
+    factors_df = pd.DataFrame(factors)
+
+    # Split for combined dataframe
+    X_train, X_test, y_train, y_test = combined_df.loc[[TRAIN_INDICES]], combined_df.loc[[TEST_INDICES]], survival_data_breast[
+        'Survival'][TRAIN_INDICES], survival_data_breast['Survival'][TEST_INDICES]
+
+    # Split for factors-only dataframe
+    X_train_factors, X_test_factors = factors_df.loc[
+        [TRAIN_INDICES]], factors_df.loc[[TEST_INDICES]]
+
+    # Model for combined dataframe
+    cox_model_combined = make_pipeline(
+        OneHotEncoder(), CoxPHSurvivalAnalysis())
+    cox_model_combined.fit(X_train, y_train)
+
+    # Model for factors-only dataframe
+    cox_model_factors = CoxPHSurvivalAnalysis()
+    cox_model_factors.fit(X_train_factors, y_train)
+
+    # Predictions and evaluations for combined dataframe
+    y_pred_combined = cox_model_combined.predict(X_test)
+    # Predictions and evaluations for factors-only dataframe
+    y_pred_factors = cox_model_factors.predict(X_test_factors)
+
+    # metrics for factors-only dataframe
+    c_index_factors = concordance_index_censored(
+        survival_data_breast['Death'][TEST_INDICES].astype(bool), y_test, y_pred_factors)[0]
+
+    # metrics for combined dataframe
+    c_index_combined = concordance_index_censored(
+        survival_data_breast['Death'][TEST_INDICES].astype(bool), y_test, y_pred_combined)[0]
+
+    metrics = {
+        'C-index_f': c_index_factors,
+        'C-index_f_c': c_index_combined,
+    }
+    return metrics
+
+
+def cox_regression_pipeline(
+    factors, clinical_features, survival_data_breast, TRAIN_INDICES, TEST_INDICES,
+        NUMERICAL_CLINICAL_FEATURES, CATEGORICAL_CLINICAL_FEATURES):
+
+    clinical_features_ = clinical_features.copy()
+    for col in NUMERICAL_CLINICAL_FEATURES:
+        clinical_features_[col] = clinical_features_[col].fillna(-1.)
+        clinical_features_[col] = clinical_features_[col].astype(float)
+
+    # Original combined dataframe
+    combined_df = pd.concat(
+        [pd.DataFrame(factors), clinical_features_], axis=1)
+    combined_df.columns = combined_df.columns.astype(str)
+
+    for col in CATEGORICAL_CLINICAL_FEATURES:
+        combined_df[col] = combined_df[col].astype('category')
+
+    # Factors-only dataframe
+    factors_df = pd.DataFrame(factors)
+    factors_df.columns = factors_df.columns.astype(str)
+
+    # Split for combined dataframe
+    event_ids_train = survival_data_breast['Death'][TRAIN_INDICES].astype(
+        bool).values
+    event_times_train = survival_data_breast['Survival'][TRAIN_INDICES].values
+
+    event_ids_test = survival_data_breast['Death'][TEST_INDICES].astype(
+        bool).values
+    event_times_test = survival_data_breast['Survival'][TEST_INDICES].values
+
+    X_train, X_test, y_train, y_test = combined_df.loc[TRAIN_INDICES], combined_df.loc[TEST_INDICES], \
+        list(zip(event_ids_train, event_times_train)), \
+        list(zip(event_ids_test, event_times_test))
+
+    y_train = np.array(y_train, dtype=[('cens', '?'), ('time', '<f8')])
+    y_test = np.array(y_test, dtype=[('cens', '?'), ('time', '<f8')])
+
+    # Split for factors-only dataframe
+    X_train_factors, X_test_factors = factors_df.loc[
+        TRAIN_INDICES], factors_df.loc[TEST_INDICES]
+    # Model for combined dataframe
+    cox_model_combined = make_pipeline(
+        OneHotEncoder(), CoxPHSurvivalAnalysis(alpha=0.5, n_iter=5000))
+    cox_model_combined.fit(X_train, y_train)
+
+    # Model for factors-only dataframe
+    cox_model_factors = CoxPHSurvivalAnalysis(alpha=0.5, n_iter=5000)
+    cox_model_factors.fit(X_train_factors, y_train)
+
+    # Predictions and evaluations for combined dataframe
+    y_pred_combined = cox_model_combined.predict(X_test)
+    # Predictions and evaluations for factors-only dataframe
+    risk_pred_factors = cox_model_factors.predict(X_test_factors)
+
+    # metrics for factors-only dataframe
+    c_index_factors = concordance_index_censored(
+        event_ids_test, event_times_test, risk_pred_factors)[0]
+    # metrics for combined dataframe
+    c_index_combined = concordance_index_censored(
+        event_ids_test, event_times_test, y_pred_combined)[0]
+
+    metrics = {
+        'C-index_f': c_index_factors,
+        'C-index_f_c': c_index_combined,
+    }
     return metrics
